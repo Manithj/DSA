bfbdc13082a40d5a23c5fc1dc40a43be3dfec384
Manith Marapperuma <marapperuma1@gmail.com>
2023-05-14 16:45:51 +0530

    Create Evaluation of Postfix Expression.java

---
+++ DataStructures/Evaluation of Postfix Expression.java
@@ -0,0 +1,57 @@
+// Java program to evaluate value of a postfix expression
+
+import java.util.Stack;
+
+class Test {
+	
+	// Method to evaluate value of a postfix expression
+	static int evaluatePostfix(String exp)
+	{
+		// Create a stack
+		Stack<Integer> stack = new Stack<>();
+
+		// Scan all characters one by one
+		for (int i = 0; i < exp.length(); i++) {
+			char c = exp.charAt(i);
+
+			// If the scanned character is an operand
+			// (number here), push it to the stack.
+			if (Character.isDigit(c))
+				stack.push(c - '0');
+
+			// If the scanned character is an operator, pop
+			// two elements from stack apply the operator
+			else {
+				int val1 = stack.pop();
+				int val2 = stack.pop();
+
+				switch (c) {
+				case '+':
+					stack.push(val2 + val1);
+					break;
+				case '-':
+					stack.push(val2 - val1);
+					break;
+				case '/':
+					stack.push(val2 / val1);
+					break;
+				case '*':
+					stack.push(val2 * val1);
+					break;
+				}
+			}
+		}
+		return stack.pop();
+	}
+
+	// Driver code
+	public static void main(String[] args)
+	{
+		String exp = "231*+9-";
+	
+		// Function call
+		System.out.println("postfix evaluation: "
+						+ evaluatePostfix(exp));
+	}
+}
+// Contributed by Sumit Ghosh
